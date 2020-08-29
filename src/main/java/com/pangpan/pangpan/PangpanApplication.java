be46ff8ae8cf2cfaec07a4eaf96c54a74cfe0d47
Easons <pp13787040125>
2020-08-29 18:10:19 +0800

    init repo

---
+++ src/main/java/com/pangpan/pangpan/PangpanApplication.java
@@ -0,0 +1,13 @@
+package com.pangpan.pangpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangpanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangpanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pangpan/pangpan/PangpanApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pangpan.pangpan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangpanApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
