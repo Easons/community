6af389e0c0a91e46f4c032c8f94e3f0d7d1dd095
Easons <2269585445@qq.com>
2020-09-01 21:22:02 +0800

    add application.properties

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ##自学的SpringBoot项目
 
 ##资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-
+[spring文档](https://spring.io/guides)  
+[springWeb](https://spring.io/guides/gs/serving-web-content/)  
+[es](https://elasticsearch.cn/explore)  
+[gitHub OAuth](https://docs.github.com/en/developers/apps/creating-an-oauth-app)
 
 ##工具
+[GIt]  
+[visual-paradigm](https://www.visual-paradigm.com/cn/download)
--- src/main/java/com/pangpan/pangpan/controller/AuthorizeController.java
+++ src/main/java/com/pangpan/pangpan/controller/AuthorizeController.java
@@ -1,4 +1,56 @@
 package com.pangpan.pangpan.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.pangpan.pangpan.dto.AccessTokenDTO;
+import com.pangpan.pangpan.dto.GitHubUser;
+import com.pangpan.pangpan.provider.GithubProvider;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private  String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private  String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+
+//        accessTokenDTO.setClient_id("Iv1.e10453b85eabeec3");
+//        accessTokenDTO.setClient_secret("8e9823f1640d6dcb306944d96adff4111bd5e474");
+//        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+
+        GitHubUser gitHubUser = githubProvider.getUser(accessToken);
+
+        System.out.println(gitHubUser.getId()+gitHubUser.getName());
+
+
+
+        return "index";
+    }
 }
