829e8f6fceb0a2eafbeb9a8c23a5dfcfe19b8d04
Easons <2269585445@qq.com>
2020-11-09 10:57:39 +0800

    update

---
+++ src/main/java/com/pangpan/pangpan/controller/PublishController.java
@@ -0,0 +1,15 @@
+package com.pangpan.pangpan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+}
